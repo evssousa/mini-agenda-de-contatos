ba2c784c80043bc2aa7b015b647bd4a6eef40f08
Everson Sousa <67715053+evssousa@users.noreply.github.com>
2025-12-13 19:31:31 +0000

    removido comentários da versão antiga

--- main.rb
+++ main.rb
@@ -34,28 +34,3 @@ while opcao != "4"
     sleep(1)
   end
 end
-
-# comando = ARGV[0]
-# args = ARGV[1..]
-
-# case comando
-# when "add"
-#   if args.size == 3
-#     Agenda.add(args[0], args[1], args[2])
-#   else
-#     puts "Modo de usar: ruby main.rb add \"Nome\" \"Telefone\" \"Email\""
-#   end
-
-# when "list"
-#   Agenda.list
-
-# when "search"
-#   if args.size == 1
-#     Agenda.search(args[0])
-#   else
-#     puts "Modo de usar: ruby main.rb search \"Nome\""
-#   end
-  
-# else
-#   puts "Comando inválido. Use: add, list ou search."
-# end
